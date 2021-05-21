e0a7e5ae29f0d77f867e53c35fb81bebbff77ecf
AndriyZakopets <zakihor3@gmail.com>
2021-05-21 17:44:35 +0300

    Update layout

--- src/components/layouts/AuthLayout.js
+++ src/components/layouts/AuthLayout.js
@@ -1,13 +1,12 @@
 import { Link } from 'react-router-dom';
 
-import Main from 'components/layout/Main';
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
 import Container from 'react-bootstrap/Container';
 import Button from 'react-bootstrap/Button';
 
+import Main from 'components/layout/Main';
 import paths from 'router/paths';
-import { logout } from 'services/AuthManager';
 
 export default function AppLayout({ user, children }) {
   return (
@@ -21,7 +20,12 @@ export default function AppLayout({ user, children }) {
           <Navbar.Toggle aria-controls="basic-navbar-nav" />
           <Navbar.Collapse id="basic-navbar-nav">
             <Nav className="ml-auto">
-              <Button onClick={() => logout()}>Log out</Button>
+              <Button as={Link} to={paths.login}>
+                Log in
+              </Button>
+              <Button className="ml-1" as={Link} to={paths.signUp}>
+                Create account
+              </Button>
             </Nav>
           </Navbar.Collapse>
         </Container>
